b2123a72487dd4e6e37be9739fdb4be31d0eed6a
Ro <147473041+444Ro666@users.noreply.github.com>
2023-10-23 21:50:29 +0000

    Add files via upload

--- ezorsia/dllmain.cpp
+++ ezorsia/dllmain.cpp
@@ -51,6 +51,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 		HookIWzNameSpace__Mount(true);
 		HookCWvsApp__InitializeResMan(false); //experimental //ty to all the contributors of the ragezone release: Client load .img instead of .wz v62~v92
 		Hook_StringPool__GetString(true); //hook stringpool modification //ty !! popcorn //ty darter
+		Hook_lpfn_NextLevel(true);
 		//Hook_get_unknown(true);
 		//Hook_get_resource_object(true); //helper function hooks  //ty teto for helping me get started
 		//Hook_com_ptr_t_IWzProperty__ctor(true);
